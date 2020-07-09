d3e07e73fe7cb702f6cec2b4d65c6115d3749617
Vitor Leal <vitor_leal2201@hotmail.com>
2020-07-09 19:27:58 -0300

    Init Project architeture and config
    Signed-off-by: Vitor Leal <vitor_leal2201@hotmail.com>

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -2,8 +2,8 @@ import {API} from './baseService/index';
 
 export async function getMovies() {
   try {
-    const response = await API.get('/movie/550?language=pt-BR');
-    //await console.log('response movies', response);
+    const response = await API.get('/search/movie?query="fast"');
+    await console.log('response movies', response);
     return Promise.resolve(response);
   } catch (error) {
     // console.log('error', error);
